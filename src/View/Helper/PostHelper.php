6c37c3d8ee380dd5645cb0e664d54874f239ff40
Daniel Pedro <danielfpedro@gmail.com>
2016-05-31 23:30:09 -0300

    titulo do parse img e parse de video

--- src/View/Helper/PostHelper.php
+++ src/View/Helper/PostHelper.php
@@ -34,8 +34,10 @@ class PostHelper extends Helper
 
     public function parseText($text)
     {
+        //$text = nl2br($text);
         $text = $this->_parseImage($text);
-        return $this->Text->autoParagraph($text);
+        $text = $this->_parseVideo($text);
+        return '<div class="text-post">' . $text . '</div>';
     }
     protected function _parseImage($text)
     {
@@ -55,12 +57,60 @@ class PostHelper extends Helper
 
         return $text;
     }
+    protected function _parseVideo($text)
+    {
+        $pattern = '/(\[video\])(\{.+\})/';
+        
+        preg_match_all($pattern, $text, $matches, PREG_OFFSET_CAPTURE);
+
+        if ($matches) {
+            foreach ($matches[0] as $key => $match) {
+                $options = json_decode($matches[2][$key][0], true);
+
+                $video = $this->_getVideo($options);
+
+                $text = str_replace($match[0], $video, $text);
+            }
+        }
+
+        return $text;
+    }
     protected function _getImage($options)
     {
         if ($options) {
             $src = $options['src'];
             unset($options['src']);
-            return $this->Html->image('../files/images/' . $src, $options);
+            $image = $this->Html->image('../files/images/' . $src, $options);
+
+            if (isset($options['data-title'])) {
+                $image .= '<div class="image-description">' . $options['data-title'] . '</div>';
+            }
+            
+            return $image;
+        }
+    }
+    protected function _getVideo($options)
+    {
+        if ($options) {
+            switch ($options['provider']) {
+                case 'youtube':
+                    $src = 'https://www.youtube.com/embed/' . $options['src'];
+                    $video = "<div class=\"video-wrap embed-responsive embed-responsive-16by9\">
+                        <iframe
+                            width=\"100%\"
+                            src=\"" . $src . "\"
+                            frameborder=\"0\"
+                            allowfullscreen>
+                        </iframe>
+                    </div>";
+                    break;
+            }
+
+            if (isset($options['data-title'])) {
+                $video .= '<div class="image-description">' . $options['data-title'] . '</div>';
+            }
+            
+            return htmlspecialchars_decode($video);
         }
     }
 
